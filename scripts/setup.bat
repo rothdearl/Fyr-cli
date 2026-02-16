923625b97a2320121f90e6987e31419df39ba1cb
Rothimus <rothdearl@gmail.com>
2026-02-16 12:49:58 -0600

    Updating the setup script.

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,5 +1,6 @@
 @echo off
 
 :: Install required packages.
-pip3 install colorama --upgrade --user %*
-pip3 install python-dateutil --upgrade --user %*
+pip3 install colorama --upgrade --user "$@"
+pip3 install python-dateutil --upgrade --user "$@"
+pip3 install requests --upgrade --user "$@"
