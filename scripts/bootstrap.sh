bef77119ef94e608a6517b864b6b7552fdf47322
Rothimus <rothdearl@gmail.com>
2026-02-22 13:54:22 -0600

    Updating the bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 # Install PyForge: use --break-system-packages on externally managed environments.
-pip3 install ../ --user
-
-# Remove build files.
-#rm -r ../pyforge.egg-info/
-#rm -r ../build/
+pip3 install ../ --user "$@"
